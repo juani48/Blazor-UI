d366ba2afe651c49617cebd9e9a3a9401574bf18
Juan Ignacio Brecevich <60331106+juani48@users.noreply.github.com>
2024-05-28 12:25:08 -0300

    Lista de clientes

---
+++ UI/Entidades/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace UI.Entidades;
+
+public class Cliente
+{
+    public string Nombre { get; set; } = "";
+    public string Apellido { get; set; } = "";
+    public int Edad { get; set; }
+
+    public static List<Cliente> GetClientes(){
+        return new List<Cliente> {
+            new Cliente() {Nombre="Pablo",Apellido="Perez", Edad=34},
+            new Cliente() {Nombre="Laura",Apellido="García", Edad=30},
+            new Cliente() {Nombre="José",Apellido="Lopez", Edad=45},
+            new Cliente() {Nombre="Ana",Apellido="Colombo", Edad=21},
+            new Cliente() {Nombre="María",Apellido="Suarez", Edad=15}};
+    }
+}
